c3d962013886524b4b797d6c36ba34ad33df9f04
rbangay98 <58495351+rbangay98@users.noreply.github.com>
2020-05-30 14:22:01 +1000

    Update ass4.py

--- ass4.py
+++ ass4.py
@@ -8,7 +8,7 @@ realID = face_recognition.face_encodings(image)[0]
 
 for filename in os.listdir(TEST_FACES):
     image = face_recognition.load_image_file(f"{TEST_FACES}/{filename}")
-    face = face_recognition.face_encodings(image, model="cnn")
+    face = face_recognition.face_encodings(image, model="cnn")[0]
     results = face_recognition.compare_faces([realID], face, 0.6)
     if True in results:
         print("Access Granted" + " " + filename)
