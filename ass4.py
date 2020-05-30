b0bb9dec8f8c1c7acb9a6b2743d47b5ebfdf931a
rbangay98 <58495351+rbangay98@users.noreply.github.com>
2020-05-30 13:47:19 +1000

    Update ass4.py

--- ass4.py
+++ ass4.py
@@ -7,12 +7,11 @@ MODEL = "cnn"
 
 
 image = face_recognition.load_image_file(f"image_0001.jpg")
-realID = face_recognition.face_encodings(image)
+realID = face_recognition.face_encodings(image)[0]
 
 for filename in os.listdir(TEST_FACES):
-    print(filename)
     image = face_recognition.load_image_file(f"{TEST_FACES}/{filename}")
-    face = face_recognition.face_encodings(image, model=MODEL)
+    face = face_recognition.face_encodings(image)[0]
     results = face_recognition.compare_faces(realID, face)
-    if True in results:
-        print("match found" + filename[results.index(True)])
+    # if True in results:
+    #     print("match found" + filename[results.index(True)])
