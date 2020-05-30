1f50ab86ff174f07134f2a48003b0cbce470b9ed
rbangay98 <58495351+rbangay98@users.noreply.github.com>
2020-05-30 13:34:13 +1000

    added faces and conde v1

--- ass4.py
+++ ass4.py
@@ -1,3 +1,22 @@
 import face_recognition
 import os
 import cv2
+
+TEST_FACES = "test_faces"
+TOLERANCE = 0.6
+MODEL = "cnn"
+
+
+image = face_recognition.load_image_file(f"image_0001")
+realID = face_recognition.face_encodings(image)[0]
+
+for filename in os.listdir(TEST_FACES):
+    print(filename)
+    image = face_recognition.load_image_file(f"{TEST_FACES}/{filename}")
+    faces = face_recognition.face_encodings(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RBG2BRG)
+
+    for face in faces:
+        results = face_recognition.compare_faces(realID, face, TOLERANCE)
+        if True in results:
+            print("match found" + filename[results.index(True)])
