318a44aac276cdc16744aae4800534e00647db6d
rbangay98 <58495351+rbangay98@users.noreply.github.com>
2020-05-30 14:20:54 +1000

    Update ass4.py

--- ass4.py
+++ ass4.py
@@ -4,8 +4,8 @@ import os
 image = face_recognition.load_image_file(f"image_0001.jpg")
 realID = face_recognition.face_encodings(image)[0]
 
-for filename in os.listdir(test_faces):
-    image = face_recognition.load_image_file(f"{test_faces}/{filename}")
+for filename in os.listdir("test_faces"):
+    image = face_recognition.load_image_file(f"{"test_faces"}/{filename}")
     face = face_recognition.face_encodings(image, model="cnn")
     results = face_recognition.compare_faces([realID], face, 0.6)
     if True in results:
